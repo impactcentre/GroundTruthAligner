e4bd1b984b8336abc14185cd02d18e4699cb7719
Antonio-M. Corbi Bellot <antonio.corbi@ua.es>
2014-05-08 16:47:06 +0200

    Deleted old code from modelview.d.

--- src/mvc/modelview.d
+++ src/mvc/modelview.d
@@ -32,57 +32,23 @@ import mvc.set;
  */
 public class Model {
   public this () {
-    //m_vl = [];
     //_vs = Set!View();
   }
 
   public void add_view (View v) { 
-    /*
-    for (int i = 0; i < m_vl.length; i++)
-      if (m_vl[i] == v) return;
-
-    m_vl ~= v;
-    */
-
     _vs ~= v;
     //v.set_model (this);
   }
 
   public void del_view (View v) { 
     _vs.remove(v);
-
-    /*
-    if (m_vl.length == 0)
-      return;
-
-    // locate v pos
-    int pos = -1;
-    for (int i = 0; i < m_vl.length; i++)
-      if (m_vl[i] == v) {
-        pos = i;
-        break;
-      }
-    if (pos != -1) { // v found
-      auto vaux = m_vl[0..pos];
-      if (pos != m_vl.length)
-        vaux ~= m_vl[pos+1..$];
-
-      m_vl = vaux;
-    }
-    */
-
   }
   
   public void notify_views () {
-    /*foreach (v ; m_vl) {
-      v.update ();
-      }*/
-
     foreach (v, val ; _vs) v.update ();
   }
 
   //-- Data ----------------------------------
-  private View[] m_vl;
   private Set!View _vs;
 }
 
